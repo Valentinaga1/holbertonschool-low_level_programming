de97165643437db85db197d90683d4cef75966dd
Valentinaga1 <1881@holbertonschool.com>
2020-06-11 19:47:31 +0000

    Ctasks0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <time.h>
 /**
-*more headers goes there
-*
-* betty style doc for function main goes there
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -16,12 +16,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	} else
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
