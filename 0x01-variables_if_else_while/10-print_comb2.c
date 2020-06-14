b5eb01c1c3ae866702cb8ae6d21cb5b98db990a5
Valentinaga1 <Valentinaga1>
2020-06-14 22:13:08 +0000

    Ctasks10Correct}

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,27 +5,25 @@
  */
 int main(void)
 {
-	int i, j;
-
-	i = 48;
-	j = 48;
-	while (i < 58)
-	{
-		while (j < 58)
-		{
-			putchar(i);
-			putchar(j);
-			if (i != 57 ||  j != 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
-			j++;
-
-		}
-		j = 48;
-		i++;
-	}
-	putchar('\n');
-	return (0);
+    int x = 48;
+    int y = 48;
+    while (x <= 57)
+    {
+        while(y <= 57)
+        {
+            putchar(x);
+            putchar(y);
+            if((x != 57) || (y != 57))
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            y++;
+        }
+        y=48;
+        x++;
+    }
+    
+    putchar('\n');
+    return (0);
 }
