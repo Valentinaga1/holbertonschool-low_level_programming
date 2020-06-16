da96836e8121df91fed598c8009deb8cf3413ac5
Valentinaga1 <Valentinaga1>
2020-06-16 17:59:33 +0000

    CTasksAbs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*_abs - prints the sign of a number.
+*@n: input character
+* Return: 0 if is zero , 1 if is positive and -1 if is negative.
+*/
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+	n = n * -1;
+	}
+	else
+	{
+	}
+	return (n);
+}
