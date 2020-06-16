30483426e881f88eae5602445b3d811a5959c1ec
Valentinaga1 <Valentinaga1>
2020-06-16 14:46:22 +0000

    CTasksAlphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ *_alphaber - prints de alphabet.
+ *@c: is the input character.
  * Return: Always 0.
  */
 void print_alphabet(void)
