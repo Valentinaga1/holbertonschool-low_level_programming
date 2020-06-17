6b130f4093228916019245468b3206ef7d1abc94
Valentinaga1 <Valentinaga1>
2020-06-17 00:53:16 +0000

    CTasks10Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*add - prints an addition between two numbers
+*@a: number 1
+*@b: number 2
+* Return: returns each minute in a specific day
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
