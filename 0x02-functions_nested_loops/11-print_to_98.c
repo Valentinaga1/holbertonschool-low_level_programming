42bac862d863231d0264a6f5d90869f5aa79f277
Valentinaga1 <Valentinaga1>
2020-06-17 00:33:27 +0000

    CTasksPrintto98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*print_to_98 - prints numbers from n to 98
+*@n: input number
+* Return: returns each minute in a specific day
+*/
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n >= 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	putchar('\n');
+}
+
