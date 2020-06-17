46f67d81950cad690c48b8be3dae5073cf2e9290
Valentinaga1 <Valentinaga1>
2020-06-17 00:47:32 +0000

    CTasksPrintTo98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,22 @@
 */
 void print_to_98(int n)
 {
-	while (n < 98)
-	{
-		printf("%d, ", n);
-		n++;
-	}
-	while (n >= 98)
-	{
-		printf("%d, ", n);
-		n--;
-	}
-	putchar('\n');
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
 }
-
+while (n >= 98)
+{
+if (n != 98)
+{
+printf("%d, ", n);
+}
+else
+{
+printf("%d", n);
+}
+n--;
+}
+putchar('\n');
+}
