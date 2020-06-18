9f8989637c05a8b991d3c3b3c7fb7a7932240e3f
Valentinaga1 <Valentinaga1>
2020-06-18 02:56:27 +0000

    _putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,28 @@
-#include "holberton.h"
-#include <stdio.h>
-/**
-*print_to_98 - prints numbers from n to 98
-*@n: input number
-* Return: returns each minute in a specific day
-*/
-void print_to_98(int n)
-{
-while (n < 98)
-{
-printf("%d, ", n);
-n++;
-}
-while (n >= 98)
-{
-if (n != 98)
-{
-printf("%d, ", n);
-}
-else
-{
-printf("%d", n);
-}
-n--;
-}
-_putchar('\n');
-}
+			#include "holberton.h"
+			#include <stdio.h>
+			/**
+			*print_to_98 - prints numbers from n to 98
+			*@n: input number
+			* Return: returns each minute in a specific day
+			*/
+			void print_to_98(int n)
+			{
+				while (n < 98)
+				{
+					printf("%d, ", n);
+					n++;
+				}
+				while (n >= 98)
+				{
+				if (n != 98)
+				{
+				printf("%d, ", n);
+				}
+				else
+				{
+				printf("%d", n);
+				}
+				n--;
+				}
+				_putchar('\n');
+				}
