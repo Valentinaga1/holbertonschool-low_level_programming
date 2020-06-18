5e28d6f2274f1355bb4e2076967497e58b56b499
Valentinaga1 <Valentinaga1>
2020-06-18 22:58:03 +0000

    CTasksPrintSquare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*print_square - draws a diagonal line on the terminal.
+*@size: size of square.
+*/
+void print_square(int size)
+{
+	int l1;
+	int l2;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	l1 = 1;
+
+	while (l1 <= size)
+	{
+		l2 = 1;
+
+		while (l2 <= size)
+		{
+			_putchar('#');
+			l2++;
+		}
+		_putchar('\n');
+		l1++;
+	}
+}
