fced93cb924ef38e99de4175d5a5e088454dff00
Valentinaga1 <Valentinaga1>
2020-06-18 18:43:01 +0000

    CTasksMultiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*mul - multiplies two integers.
+*@a: number input.
+*@b: number input.
+*Return: the result of the multiplication.
+*/
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
