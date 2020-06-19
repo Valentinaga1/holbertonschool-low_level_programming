17c543cdd878fbdc42636e6b2273f388c947efa8
Valentinaga1 <Valentinaga1>
2020-06-19 02:39:10 +0000

    CTasksTriangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*print_triangle - draws a triangle.
+*@size: size of square.
+*/
+void print_triangle(int size)
+{
+int a, b;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (a = 1; a <= size; a++)
+{
+for (b = 1; b <= size; b++)
+{
+if (b <= (size - a))
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
