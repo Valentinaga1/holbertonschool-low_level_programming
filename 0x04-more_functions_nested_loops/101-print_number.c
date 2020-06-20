1b929121188b8761fd9f79b5383992e342e22e93
Valentinaga1 <Valentinaga1>
2020-06-20 22:43:12 +0000

    CTAdvacancedTasksPrintnumber

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+*print_number -  function that prints an integer.
+*@n: n to print.
+*/
+void print_number(int n)
+{
+	char lastDigit;
+	long reversed;
+
+if (n < 0)
+{
+_putchar('-');
+lastDigit = ('0' - (n % 10));
+n /= -10;
+}
+else
+{
+lastDigit = ((n % 10) + '0');
+n /= 10;
+}
+
+reversed = 0;
+while (n > 0)
+{
+reversed = reversed * 10 + (n % 10);
+n /= 10;
+}
+
+while (reversed > 0)
+{
+char c = ((reversed % 10) + '0');
+_putchar(c);
+reversed /= 10;
+}
+_putchar(lastDigit);
+}
