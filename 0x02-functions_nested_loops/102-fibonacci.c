17b9da46fa8c692ade59cdbfd72917b42b1c7695
Valentinaga1 <Valentinaga1>
2020-06-21 23:11:20 +0000

    prints the first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+*followed by a new line.
+* Return: returns each minute in a specific day
+*/
+int main(void)
+{
+
+	int n = 0;
+	long fn;
+	long fn1 = 0;
+	long fn2 = 1;
+
+		while (n < 50)
+		{
+			fn = fn1 + fn2;
+			printf("%ld\n", fn);
+			fn1 = fn2;
+			fn2 = fn;
+			n++;
+		}
+		return (0);
+}
