5843df3b387473094c05160a3b046304fb868e07
Valentinaga1 <Valentinaga1>
2020-06-23 15:16:28 +0000

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -15,3 +15,13 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
+/*Anohter  way: bassically the only thing is changing is the way you can write a string,
+you can write it like this str[i] or like *(str + i) in case you want to go over the string.
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');*/
