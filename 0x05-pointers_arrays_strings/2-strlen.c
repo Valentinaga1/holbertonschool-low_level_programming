a5cdbd5304f4afeb9c9dca5d565a69e0f69fea63
Valentinaga1 <Valentinaga1>
2020-06-23 15:03:07 +0000

    Length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,3 +12,14 @@ int _strlen(char *s)
 	i++;
 	return (i);
 }
+/*Another way to solve it. REMEMBER: Null is the end of a string and it is implicit ,so
+the condition while (*(s + i)) is saying that is different to null or != 0.
+REMEMBER: You can write a string like char s[i] or like char *(s+i).
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	return (i);
+}*/
