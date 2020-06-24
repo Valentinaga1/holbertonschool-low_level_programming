12733dc7c4cfd039f40aac2caef295e8ab8af670
Valentinaga1 <Valentinaga1>
2020-06-24 18:40:17 +0000

    Convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,11 +17,11 @@ int _atoi(char *s)
 		{
 			aux = aux * 1;
 		}
-			i++;
-	}
-		while(s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = (num * 10) + (s[i] - '0');
 		}
+		i++;
+	}
 	return (num * aux);
 }
