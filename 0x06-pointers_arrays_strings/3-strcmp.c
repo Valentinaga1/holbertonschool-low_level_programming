9242e774c4ea4e0fda0fe3795a5a1ea669e961e9
Valentinaga1 <Valentinaga1>
2020-06-25 21:36:12 +0000

    compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings.
+ * @s1: string 1
+ * @s2: cstring 2
+ * Return: a number positive, negative or zero.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 >= *s2)
+		{
+			return (*s1 - *s2);
+		}
+		if (*s1 < *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
+}
