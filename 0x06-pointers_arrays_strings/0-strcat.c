5ab3c6a845fe86dbeb28e43a7054fc8ba5f8bd98
Valentinaga1 <Valentinaga1>
2020-06-25 18:23:28 +0000

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,9 @@ char *_strcat(char *dest, char *src)
 	i = j = 0;
 	while (*(dest + i) != '\0')
 		i++;
-	while (*(src + j) != '\0' )
+	while (*(src + j) != '\0')
 	{
-		(*(dest + i)) = (*(src + j));
+		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
