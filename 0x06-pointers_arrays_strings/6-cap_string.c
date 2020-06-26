1ac8362335182051bc4ff697a4c621f66daf70f3
Valentinaga1 <Valentinaga1>
2020-06-26 02:30:41 +0000

    capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,9 +22,9 @@ char *cap_string(char *s)
 		{
 			s[i] = s[i] - 32;
 		}
-		else if ((s[0] >= 'a' && s[0] <= 'z'))
+		else if ((s[i] >= 97 && s[i] <= 122) && i == 0)
 		{
-			s[0] = s[0] -32;
+			s[i] = s[i] -32;
 		}
 		else
 		{
