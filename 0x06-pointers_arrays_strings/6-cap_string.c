32210c3ec8852576520067e6213bfdc79869abc3
Valentinaga1 <Valentinaga1>
2020-06-26 01:51:30 +0000

    capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] > 'a' && s[i] < 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+		i++;
+	}
+	return (s);
+}
