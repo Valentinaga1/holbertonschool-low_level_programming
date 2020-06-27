57617480abb23ef3e738778e7c5dec6a8b37607e
Valentinaga1 <Valentinaga1>
2020-06-27 02:47:21 +0000

    ncodes a string using rot13.

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -15,9 +15,9 @@ char *rot13(char *s)
 		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-				s[i] = s[i] - 13;
+				s[i] = (s[i] - 13);
 			else
-				s[i] = s[i] + 13;
+				s[i] = (s[i] + 13);
 			i++;
 		}
 	i++;
