b529903570d90b9c2aa3645ee64e5a7f166fd2cf
Valentinaga1 <Valentinaga1>
2020-06-29 19:36:49 +0000

    locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,25 +3,27 @@
  * _strstr - locates a substring.
  * @haystack: string
  * @needle: substring
- * Return: substring
+ * Return: haystack
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-
+	i = 0;
 	while (haystack[i] != '\0')
 	{
-		while (haystack[i] != '\0' && needle[j] != '\0' && haystack[i] == needle[j])
+		j = 0;
+		while (needle[j] == haystack[i] && needle[j] != '\0' && haystack[i] != '\0')
 		{
+			if (needle[j + 1] != haystack[i + 1])
+			{
+				break;
+			}
 			return (haystack + i);
 			j++;
-		}
-		if (needle[j] == '\0')
-		{
-			return (haystack + i);
+			i++;
 		}
 		i++;
 	}
-	return ('\0');
+	return (0);
 }
