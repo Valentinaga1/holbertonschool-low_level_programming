c592499f166001ce22cada7981cdb45c743c2620
Valentinaga1 <Valentinaga1>
2020-06-29 17:09:01 +0000

    locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring.
+ * @haystack: string
+ * @needle: substring
+ * Return: substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		if (needle[j] == haystack[i])
+		{
+			return (needle);
+		}
+		i++;
+	}
+	return (0);
+}
