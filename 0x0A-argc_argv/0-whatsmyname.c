4eb5d77c24d72a47fc763223bedb93d66700c86b
Valentinaga1 <Valentinaga1>
2020-07-05 17:08:50 +0000

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: number of arguments.
+ * @argv: argument vecotr
+ * Return: zero
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
