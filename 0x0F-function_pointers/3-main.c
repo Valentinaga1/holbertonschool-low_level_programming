f3b23612817121daa37f2b1ada2d40f1a5cf7539
Valentinaga1 <Valentinaga1>
2020-07-16 03:14:18 +0000

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,12 +14,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
-			&& *argv[2] != '/' &&  *argv[2] != '%')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	if (get_op_func(argv[2]) == NULL)
+		printf("Error\n"), exit(99);
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
