4c0343f0aeb2d9102f9d8cf77f78d4c820f665de
Valentinaga1 <Valentinaga1>
2020-07-16 01:19:12 +0000

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+/**
+ * main - check the code
+ * @argc: cantidad de arguments
+ * @argv: argumentos
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+			&& *argv[2] != '/' &&  *argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[1] == '/' || *argv[1] == '%') && *argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", get_op_func(argv[2])(a, b));
+
+	return (0);
+}
