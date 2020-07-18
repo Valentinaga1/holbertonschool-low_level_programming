0112c14017c220c46a40d8e92d6e5f550b95d772
Valentinaga1 <Valentinaga1>
2020-07-18 23:11:33 +0000

    Variadic Functions Header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct MyPrintf - Struct for my printf 
+ *
+ * @tipo: identifier of function to call
+ * @f: The function associated
+ */
+typedef struct MyPrintf
+{
+	char *tipo;
+	void (*f)(va_list);
+}Myp;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
