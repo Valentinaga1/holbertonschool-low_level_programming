6078fe2c31f27a828a4f4d30aea36d9c36dffbe2
Valentinaga1 <1881@holbertonschool.com>
2020-07-28 15:01:51 +0000

    rees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - function that frees a listint_t list.
+ * @head: Pointer to the first element of a linked list.
+ * Return: nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
