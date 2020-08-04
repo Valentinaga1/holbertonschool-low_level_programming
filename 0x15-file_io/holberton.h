ae192253d281d804a8c8a03ad12527622201fb1f
Valentinaga1 <1881@holbertonschool.com>
2020-08-04 20:25:29 +0000

    Holberton Header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,7 @@
 #define _HOLBERTON_H_
 
 #include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
