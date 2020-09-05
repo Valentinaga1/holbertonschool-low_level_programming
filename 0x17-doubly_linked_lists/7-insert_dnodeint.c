1e4f436f85d7669298f48a37b76a48475063189b
Valentinaga1 <1881@holbertonschool.com>
2020-09-05 17:41:45 +0000

    Function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,13 +18,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;/*Asigno data al nuevo nodo*/
 	if (idx == 0)/*si index 0, agrego nodo al inicio*/
 	{
-		if (*h != NULL)
-			(*h) ->prev = new_node;
 		add_dnodeint_end(h, n);
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		return (new_node);
 	}
 	temp = *h;
-	while ((posicion < idx) && (temp != NULL))
+	while ((temp != NULL) && (posicion < idx))
 	{
 		temp = temp->next;
 		posicion++;
