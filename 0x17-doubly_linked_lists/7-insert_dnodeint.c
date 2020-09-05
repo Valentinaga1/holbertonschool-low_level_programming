e986ac5ac9c8b0fc1d8c4085871fa846ce6a054f
Valentinaga1 <1881@holbertonschool.com>
2020-09-05 17:36:34 +0000

    Function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,28 +16,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;/*Asigno data al nuevo nodo*/
-	new_node->next = NULL;/*inicializo nodo*/
-	new_node->prev = NULL;/*inicializo nodo*/
-	if (h == NULL)
-		return (NULL);
 	if (idx == 0)/*si index 0, agrego nodo al inicio*/
 	{
 		add_dnodeint_end(h, n);
 		return (new_node);
 	}
-	else
+	temp = *h;
+	while ((posicion < idx) && (temp != NULL))
 	{
-		temp = *h;
-		while (posicion < idx)
-		{
-			temp = temp->next;
-			posicion++;
-		}
-		new_node->next = temp->next; /*Conecto con el nodo n+1*/
-		new_node->prev = temp;/*Conecto con el nodo n-1*/
-		if(temp->next != NULL)
-			temp->next->prev = new_node;/*Conecto n+1 con el nuevo nodo*/
-		temp->next = new_node;/*Conecto n-1 con el nuevo nodo*/
+		temp = temp->next;
+		posicion++;
 	}
+	if (temp == NULL)
+		return (NULL);
+	new_node->next = temp->next; /*Conecto con el nodo n+1*/
+	new_node->prev = temp;/*Conecto con el nodo n-1*/
+	if (temp->next != NULL)
+		temp->next->prev = new_node;/*Conecto n+1 con el nuevo nodo*/
+	temp->next = new_node;/*Conecto n-1 con el nuevo nodo*/
 	return (new_node);
 }
