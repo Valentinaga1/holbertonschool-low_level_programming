a7d2c5f379d40af49714c3617fee19d6bf5ccec1
Valentinaga1 <1881@holbertonschool.com>
2020-10-02 17:43:42 +0000

    Function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
+
 	if (!size)
-		return 0;
-	/*cuanod creamos la tabla no sabemos cuanta dta tiene, pero cuando creamos el array ya sabemos*/
+		return (0);
 	new_ht = malloc(sizeof(hash_table_t));
 	if (!new_ht)
-		return 0;
+		return (0);
 	new_ht->size = size;
 
-	/*es más optimo que usar malloc, porque necesitaríamos un for para iniciliazar */
 	new_ht->array = calloc(size, sizeof(hash_node_t *));
-	if(!new_ht->array)
+	if (!new_ht->array)
 	{
 		free(new_ht);
-		return(NULL);
+		return (NULL);
 	}
 	return (new_ht);
 }
